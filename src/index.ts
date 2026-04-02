98fce3f832562e000639e96fbcff0b26170a8cda
mochamap1e <mcmicrosoft5@example.com>
2026-04-01 20:47:19 -0400

    uggggghhh too complex

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 // https://wyliemaster.github.io/gddocs/#/endpoints/endpoints
 
 import fs from "fs";
+import path from "path";
 import axios from "axios";
 import { Elysia } from "elysia";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const endpointHandlers: Record<string, any> = {};
+
+for (const endpoint of fs.readdirSync(path.join(__dirname, "endpoints"))) {
+    const module = await import(path.join(__dirname, "endpoints", endpoint));
+    endpointHandlers[endpoint.replace(".ts", "")] = module.default;
+}
 
 const PORT = 4500;
 
@@ -16,16 +28,18 @@ new Elysia({
 })
     .post("*", async ({ request, body }) => {
         const endpoint = new URL(request.url).pathname.replace("/", "").replace(".php", "");
+        console.log("path:", endpoint);
+
+        if (endpointHandlers[endpoint]) {
+            return endpointHandlers[endpoint](body);
+        }
+
+        // @ts-ignore
+        const fixedBody = new URLSearchParams(Object.fromEntries(Object.entries(body).map(([key, value]) => [key, Array.isArray(value) ? value.join(",") : value]))).toString();
         const url = `http://www.boomlings.com/database/${endpoint}.php`;
 
-        console.log("path:", endpoint);
-        console.log("url:", url);
-        console.log("body:", body);
-
-        body = new URLSearchParams(Object.fromEntries(Object.entries(body).map(([key, value]) => [key, Array.isArray(value) ? value.join(",") : value]))).toString();
-
         try {
-            const response = await axios.post(url, body, {
+            const response = await axios.post(url, fixedBody, {
                 validateStatus: () => true,
                 headers: {
                     "User-Agent": "",
@@ -35,34 +49,12 @@ new Elysia({
 
             const code = await response.status;
             console.log("GD response code:", code);
-            console.log("GD response data:", response.data)
+            console.log("GD response data:", response.data);
             return response.data;
         } catch(error) {
             console.error(error);
             return null;
         }
-
-        /*
-        switch (endpoint) {
-            case "getGJDailyLevel": // Returns the index of the current daily level and the time left in seconds, separated by a pipe.
-                return "1|1";
-            default:
-                const response = await fetch(`https://www.boomlings.com/database/${endpoint}`, {
-                    method: "POST",
-                    headers: {
-                        "User-Agent": "",
-                        "Content-Type": "application/x-www-form-urlencoded"
-                    },
-                    body: body as any
-                });
-
-                const responseText = await response.text();
-
-                console.log("GD response:", responseText);
-
-                return responseText;
-        }
-        */
     })
     .listen(PORT);
 
